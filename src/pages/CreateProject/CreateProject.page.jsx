fec253d5584ae72ca524a5acebfbaa07af6a3ce4
jt-96 <jetorrico@outlook.com>
2023-02-16 16:53:14 -0300

    feature(front): add projectImages state, add handlers to edit, add-remove fields, add map projectImages array

--- src/pages/CreateProject/CreateProject.page.jsx
+++ src/pages/CreateProject/CreateProject.page.jsx
@@ -11,6 +11,10 @@ function CreateProjectPage() {
         project_type: ''
     })
 
+    const [projectImages, setProjectImages] = useState([
+        { imageURL: '', dimensions: 0 }
+    ])
+
     function handleProjectDetails(e) {
 
         const { name, value } = e.target;
@@ -21,6 +25,33 @@ function CreateProjectPage() {
                 [name]: value
             }
         })
+    }
+
+    function handleImagesUpload(index, event) {
+
+        const values = [...projectImages];
+        const updatedValue = event.target.name;
+        values[index][updatedValue] = event.target.value;
+        setProjectImages(values);
+    }
+
+    function addField(e) {
+        e.preventDefault();
+        let newField = { imageURL: '', dimensions: 0 }
+        setProjectImages([...projectImages, newField])
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(projectDetails)
+        console.log(projectImages)
+    }
+
+    function removeField(index, event) {
+        event.preventDefault();
+        let data = [...projectImages];
+        data.splice(index, 1);
+        setProjectImages(data);
     }
 
     // useEffect(() => {
@@ -42,6 +73,24 @@ function CreateProjectPage() {
                     <input name="project_videoURL" value={projectDetails.project_videoURL} placeholder='Enter video URL' onChange={handleProjectDetails} />
                     <input name="project_type" value={projectDetails.project_type} placeholder='Enter type of project' onChange={handleProjectDetails} />
                 </div>
+
+                {
+                    projectImages.map((img, index) => (
+                        <div key={index}>
+                            <input type="file" id="img" name="img" accept="image/*" />
+                            <input name="imageURL" value={img.imageURL} placeholder='Image URL goes here' onChange={event => handleImagesUpload(index, event)} />
+                            <select id='dimensions' name='dimensions' onChange={event => handleImagesUpload(index, event)}>
+                                <option value={0}>2x2</option>
+                                <option value={1}>4x4</option>
+                                <option value={2}>8x8</option>
+                            </select>
+                            <button onClick={event => removeField(index, event)}>Remove</button>
+                        </div>
+                    ))
+                }
+
+                <button onClick={addField}>Add Field</button>
+                <button onClick={handleSubmit}>Submit</button>
             </form>
         </div>
 
