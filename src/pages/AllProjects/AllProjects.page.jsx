866d20502d26be729ad2eab1e2450922996b11c9
jt-96 <jetorrico@outlook.com>
2023-02-22 13:18:46 -0300

    feature(front): add projects state, add dataService, add hook for data retrieval, for value control

--- src/pages/AllProjects/AllProjects.page.jsx
+++ src/pages/AllProjects/AllProjects.page.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DataService from '../../data-service/DataService';
 
 function AllProjectsPage() {
 
+    const [projects, setProjects] = useState([]);
+    const dataService = new DataService();
+
+    useEffect(() => {
+        dataService.getAllProjects().then((response) => setProjects(response));
+    }, [])
+
+    useEffect(() => {
+        console.log(projects)
+    }, [projects])
+
     return (
-        <h1>This is AllProjectsPage!</h1>
+        <div>
+            <h1>This is AllProjectsPage!</h1>
+        </div>
     )
 }
 
