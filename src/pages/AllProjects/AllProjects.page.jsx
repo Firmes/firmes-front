ce8a158210645ad4b0233bba0c9bdcda716babec
jt-96 <jetorrico@outlook.com>
2023-02-24 12:35:36 -0300

    feature(front): add table structure, map data in table

--- src/pages/AllProjects/AllProjects.page.jsx
+++ src/pages/AllProjects/AllProjects.page.jsx
@@ -15,8 +15,36 @@ function AllProjectsPage() {
     }, [projects])
 
     return (
-        <div>
-            <h1>This is AllProjectsPage!</h1>
+        <div className="w-full flex justify-center">
+            <table className="w-9/12 table-fixed">
+                <thead>
+                    <tr>
+                        <th>Project ID</th>
+                        <th>Title</th>
+                        <th>Type</th>
+                        <th>Client</th>
+                        <th>Description</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        projects.map((p, index) => (
+                            <tr key={index} className="text-center">
+                                <td>{p.project_info_id}</td>
+                                <td>{p.project_title}</td>
+                                <td>{p.project_type}</td>
+                                <td>{p.project_client}</td>
+                                <td>{p.project_description}</td>
+                                <td className="flex justify-around">
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
