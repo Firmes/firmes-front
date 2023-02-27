ef27a8578318b261cddb3fce9f00255fe518ccff
jt-96 <jetorrico@outlook.com>
2023-02-27 17:13:40 -0300

    feature(front): add inputs, add states, refs, add imports

--- src/pages/AdminLogin/AdminLogin.page.jsx
+++ src/pages/AdminLogin/AdminLogin.page.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DataService from '../../data-service/DataService';
 
 function AdminLoginPage() {
 
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const navigate = useNavigate();
+    const dataService = new DataService();
+
     return (
         <>
+            <div>
+                <form>
+                    <div>
+                        <input type="text" placeholder='Enter username' />
+                        <input type="password" placeholder='Enter password' />
+                    </div>
+                    <div>
+                        <button>Login</button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
