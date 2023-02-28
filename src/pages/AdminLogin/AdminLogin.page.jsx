8f42356855b273330523611b7e20a21493eccc7d
jt-96 <jetorrico@outlook.com>
2023-02-28 17:44:16 -0300

    feature(front): add user context, add new account boolean, add hook to navigate when user exists, add handler func to signup or login depending on newAccount value, add handleCreateAccount handler, add form style, add conditionals to display certain titles if one is creating or login account, add conditionals to display errors

--- src/pages/AdminLogin/AdminLogin.page.jsx
+++ src/pages/AdminLogin/AdminLogin.page.jsx
@@ -1,31 +1,94 @@
-import React, { useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useRef, useState, useContext, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { UserContext } from '../../App';
 import DataService from '../../data-service/DataService';
 
 function AdminLoginPage() {
 
     const usernameRef = useRef();
     const passwordRef = useRef();
+    const { user, setUser } = useContext(UserContext);
     const [isLoading, setIsLoading] = useState(false);
+    const [isNewAccount, setIsNewAccount] = useState(useLocation().state);
     const [error, setError] = useState(false);
     const navigate = useNavigate();
     const dataService = new DataService();
 
+    useEffect(() => {
+
+        if (user == 401 || user == 400) {
+            setError(true);
+            setIsLoading(false);
+
+        } else if (user) {
+            setError(false);
+            navigate('/all-projects', {replace: true})
+        }
+
+    }, [user])
+
     function handleLogin(e) {
         e.preventDefault();
-        console.log(usernameRef.current.value, passwordRef.current.value)
+
+        if (/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(passwordRef.current.value)) {
+            setError(false);
+
+            if (isNewAccount) {
+                alert('Account created!')
+                dataService.signup(usernameRef.current.value, passwordRef.current.value).then((response) => setUser(response))
+            } else {
+                alert('Welcome!')
+                dataService.login(usernameRef.current.value, passwordRef.current.value).then((response) => setUser(response));
+            }
+
+        } else {
+            setError(true);
+        }
+    }
+
+    function handleCreateAccount(e) {
+        e.preventDefault();
+        navigate('/admin-create', { state: true, replace: true })
+        window.location.reload();
     }
 
     return (
         <>
-            <div>
-                <form>
-                    <div>
-                        <input type="text" placeholder='Enter username' name={usernameRef} ref={usernameRef}/>
-                        <input type="password" placeholder='Enter password' name={passwordRef} ref={passwordRef}/>
+            <div className="w-full h-full flex flex-col self-center pt-36">
+                {
+                    isNewAccount ?
+
+                        <span className="self-center">Create New Account</span>
+                        :
+                        <span className="self-center">Login</span>
+
+                }
+                <form className="flex justify-center flex-col">
+                    <div className="flex flex-col items-center">
+                        <input className="py-3 my-3 w-1/4" type="text" placeholder='Enter username' name='usernameRef' ref={usernameRef} />
+                        <input className="py-3 my-3 w-1/4" type="password" placeholder='Enter password' name='passwordRef' ref={passwordRef} />
+                        <span>Password must contain at least one number, symbol, capital letter, and must be 8 character minimum.</span>
                     </div>
-                    <div>
-                        <button onClick={handleLogin}>Login</button>
+                    <div className="flex flex-col items-center">
+                        {
+                            error ?
+                                <span>Username and/or Password is not valid, please try again.</span>
+                                :
+                                null
+                        }
+                        {
+                            isNewAccount ?
+                                <>
+                                    <button className="py-3 my-5 hover:text-sky-500" onClick={handleLogin}>Submit</button>
+                                </>
+
+                                :
+
+                                <>
+                                    <button className="py-3 my-5 hover:text-sky-500" onClick={handleLogin}>Submit</button>
+                                    <button className="py-1 my-1 hover:text-sky-500" onClick={handleCreateAccount}>Create Account</button>
+                                </>
+                        }
                     </div>
                 </form>
             </div>
