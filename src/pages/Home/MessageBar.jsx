4e46c61900b1df3918815db15578785db5565f90
Rodrigo <rodridega@gmail.com>
2023-03-02 12:04:42 -0300

    style: refactor font size on message bar

--- src/pages/Home/MessageBar.jsx
+++ src/pages/Home/MessageBar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export const MessageBar = () => {
   return (
-    <section className="h-8 md:flex w-full max-w-full bg-purple-700 p-2 items-center justify-center fixed z-10  bottom-0 overflow-hidden ">
-      <p className="hidden  md:block text-sm text-center md:text-3xl font-extrabold">LIVE#&*ABSTRACT~IDEAS*%L4T1N0’;[CREATIVE CREATURES%%IMPACT SEEKERS&//CHANGEMAKERS+/.EMPATHY&LOVE]=1EVOLV3
+    <section className="h-8 md:flex w-full max-w-full bg-purple-700 p-2 items-center justify-center fixed z-40  bottom-0 overflow-hidden ">
+      <p className="hidden  md:block text-xs text-center lg:text-xl xl:text-2xl font-extrabold">LIVE#&*ABSTRACT~IDEAS*%L4T1N0’;[CREATIVE CREATURES%%IMPACT SEEKERS&//CHANGEMAKERS+/.EMPATHY&LOVE]=1EVOLV3
       </p>
-      <p className="animation-message-bar md:hidden text-sm">
+      <p className="animation-message-bar sm:hidden text-sm">
       BOGOTA ™£4& MADRID ^&81 NEW YORK #$(67 ||| LIVE NOW ON YOUTUBE STREAM ||| SALUDOS
       </p>
     </section>
