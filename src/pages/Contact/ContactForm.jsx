c407e50991d238396d3a135dfaa1bee5b48a5d49
Rodrigo <rodridega@gmail.com>
2023-03-06 13:22:54 -0300

    add function to post message on DB

--- src/pages/Contact/ContactForm.jsx
+++ src/pages/Contact/ContactForm.jsx
@@ -13,11 +13,9 @@ export const ContactForm = () => {
         subject: "",
         message: ""
     })
+    const [error, setError] = useState("")
+    const [success, setSuccess] = useState("")
 
-    console.log(contactFormValues);
-    const onSubmit = () => {
-
-    }
 
 
 
@@ -27,24 +25,28 @@ export const ContactForm = () => {
             <div className='w-full md:w-4/5'>
                 <form className='p-2 md:p-4 mb-4 md:w-1/2 bg-firmes-form w-full relative'>
                     <ContactInput name={"firstName"} label={"First Name"} required={false} setContactFormValues={setContactFormValues} contactFormValues={contactFormValues} />
-
                 </form>
                 <form className='p-2 md:p-4 mb-4 md:w-1/2 bg-firmes-form w-full relative md:bottom-20 md:left-1/3'>
-                    <ContactInput name={"lastName"} label={"Last Name"} required={false} setContactFormValues={setContactFormValues} contactFormValues={contactFormValues}/>
+                    <ContactInput name={"lastName"} label={"Last Name"} required={false} setContactFormValues={setContactFormValues} contactFormValues={contactFormValues} />
                 </form>
             </div>
+
             <div className='w-full md:w-4/5 '>
                 <form className='p-2 md:p-4 mb-4 md:w-1/2 bg-firmes-form w-full relative'>
-                    <ContactInput name={"email"} label={"Email *"} required={true} setContactFormValues={setContactFormValues} contactFormValues={contactFormValues}/>
-
+                    <ContactInput name={"email"} label={"Email *"} required={true} setContactFormValues={setContactFormValues} contactFormValues={contactFormValues} />
                 </form>
                 <form className='p-2 md:p-4 mb-4 md:w-1/2 bg-firmes-form w-full relative md:bottom-20 md:left-1/3'>
-                    <ContactInput name={"subject"} label={"Subject *"} required={true} setContactFormValues={setContactFormValues} contactFormValues={contactFormValues}/>
+                    <ContactInput name={"subject"} label={"Subject *"} required={true} setContactFormValues={setContactFormValues} contactFormValues={contactFormValues} />
                 </form>
             </div>
             <div className='w-full md:w-4/5 flex flex-col md:flex-row justify-center items-center'>
                 <ContactTextArea name={"message"} label={"Subject *"} required={true} setContactFormValues={setContactFormValues} contactFormValues={contactFormValues} />
-                <SubmitButton />
+
+                <div className='flex flex-col text-xl'>
+                    {error && <p className='text-white '>{error.toUpperCase()} </p>}
+                    {success && <p className='text-emerald-400'>{success.toUpperCase()} </p>}
+                    <SubmitButton contactFormValues={contactFormValues} setError={setError} setSuccess={setSuccess} />
+                </div>
             </div>
         </>
     )
--- src/pages/Contact/SubmitButton.jsx
+++ src/pages/Contact/SubmitButton.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import DataService from '../../data-service/DataService'
 
-export const SubmitButton = () => {
+export const SubmitButton = ({ contactFormValues, setError, setSuccess }) => {
+
+    const dataService = new DataService
+
+    const onSubmit = () => {
+        dataService.saveContact(contactFormValues)
+            .then((res) => {
+                setSuccess(res.message)
+                setError("")
+            })
+            .catch((err) => {
+                setError(err.response.data.errorMessage)
+                setSuccess("")
+            })
+    }
+
     return (
-        <button className='w-full border-2 md:w-auto mb-4 bg-firmes-form p-4 text-xl rounded-md hover:border-2 hover:border-white hover:bg-transparent hover:text-firmes-form   transition-all ease-in-out duration-500'>SUBMIT</button>
+        <button className='w-full border-2 md:w-auto mb-4 bg-firmes-form p-4 text-xl rounded-md hover:border-2 hover:border-white hover:bg-transparent hover:text-firmes-form   transition-all ease-in-out duration-500' onClick={onSubmit}>SUBMIT</button>
     )
 }
