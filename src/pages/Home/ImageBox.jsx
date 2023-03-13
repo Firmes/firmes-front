91e97b1465e3f1e12b235fce6a7c8278ee9a9872
Rodrigo <rodridega@gmail.com>
2023-03-13 20:05:05 -0300

    refactor: change animations on mobile images

--- src/pages/Home/ImageBox.jsx
+++ src/pages/Home/ImageBox.jsx
@@ -2,11 +2,11 @@ import React, { useContext } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { NavContext } from '../../context/NavContext'
 
-export const ImageBox = ({ src, alt, title, id }) => {
+export const ImageBox = ({ src, alt, title, id, client }) => {
 
     const { isNavOpen } = useContext(NavContext)
 
-    const translateClasses = ['translate-y-40 translate-x-40', 'translate-y-4 -translate-x-4', 'translate-x-4', 'translate-x-4 -translate-y-40']
+    const translateClasses = ['translate-y-40 translate-x-40', 'translate-y-4 -translate-x-4', 'translate-x-4', 'translate-x-4 -translate-y-4']
     const justifySelfGrid = ['justify-self-start', 'justify-self-end', 'justify-self-center', 'justify-self-stretch']
     const alignSelfGrid = ['self-start', 'self-end', 'self-center', 'self-stretch', 'self-baseline']
     const animations = ['flip-in-diag-2-br', 'flip-in-diag-2-tl', 'flip-in-ver-right']
@@ -36,9 +36,10 @@ export const ImageBox = ({ src, alt, title, id }) => {
     }
 
     return (
-        <div className={` w-${randomWidthNumber}/6 relative my-2 text-center cursor-pointer duration-1000 transition-all ${justifySelfGrid[randomJustifySelf]} ${alignSelfGrid[randomAlignSelf]} ${translateClasses[randomTranslate]}  ${isNavOpen && isNavTranslates[randomNavTranslates]}  hover:scale-105 hover:z-40 `} onClick={handlePick} >
+        <div className={` w-${randomWidthNumber}/6 relative my-2 text-center cursor-pointer duration-1000 transition-all ${justifySelfGrid[randomJustifySelf]} ${alignSelfGrid[randomAlignSelf]} ${translateClasses[randomTranslate]}  ${isNavOpen && isNavTranslates[randomNavTranslates]}  hover:scale-105 hover:z-40`} onClick={handlePick} >
             <img src={src} alt={alt} className='w-full object-cover' />
-            <div className='text-white font-average-sans text-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>{title} </div>
+            <div className='absolute w-full h-full top-0 left-0 opacity-80 hover:bg-firmes-black transition-all duration-1000'></div>
+            <div className='text-white font-average-sans text-3xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>  {client}</div>
         </div>
     )
 }
--- src/pages/Home/ImagesMobileContainer.jsx
+++ src/pages/Home/ImagesMobileContainer.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom'
 
 import { NavContext } from '../../context/NavContext'
 import { ImageBox } from './ImageBox'
+import { MobileImageBox } from './MobileImageBox'
 
 export const ImagesMobileContainer = ({ data }) => {
     const { isNavOpen } = useContext(NavContext)
@@ -11,16 +12,16 @@ export const ImagesMobileContainer = ({ data }) => {
 
 
     return (
-        <div className=' sm:hidden p-4 w-screen grid-cols-2 overflow-hidden' >
+        <div className=' sm:hidden  p-4 w-screen grid-cols-1 overflow-hidden' >
 
             {data.map((project) => {
-                if(location.pathname === "/work" ) return <ImageBox key={project?.project_info_id} id={project?.project_info_id} title={project?.project_title} src={project.project_images[Math.floor(Math.random() * project.project_images.length)]?.project_image_url} alt={data[0]?.project_description} />
+                if (location.pathname === "/work") return <MobileImageBox key={project?.project_info_id} id={project?.project_info_id} title={project?.project_title} src={project.project_images[Math.floor(Math.random() * project.project_images.length)]?.project_image_url} alt={data[0]?.project_description} />
 
                 if (data.indexOf(project) === 1) {
                     return (
                         <>
-                            <ImageBox key={project?.project_title} id={project?.project_info_id} title={project?.project_title} src={project.project_images[Math.floor(Math.random() * project.project_images.length)]?.project_image_url} alt={data[0]?.project_description} />
-                            <div className={`w-3/5 md:w-1/6 text-white relative z-30 md:text-black md:bg-slate-300 my-4 text-box grid-item-2 duration-1000 ${isNavOpen ? 'translate-y-32 bg-slate-300 text-black p-4' : 'translate-y-0'}  cursor-pointer hover:scale-105 transition-all`} >
+                            <MobileImageBox key={project?.project_title} id={project?.project_info_id} title={project?.project_title} src={location.pathname !== "/work" ? project?.project_images.filter((image) => image.image_is_portrait)[0].project_image_url : project.project_images[Math.floor(Math.random() * project.project_images.length)]?.project_image_url} alt={data[0]?.project_description} />
+                            <div className={`w-3/5 md:w-1/6 bg-slate-300 text-black p-4 relative z-30  my-4 text-box grid-item-2 duration-1000 ${isNavOpen ? 'translate-y-32 ' : 'translate-y-0'}  cursor-pointer hover:scale-105 transition-all`} >
                                 <p className='text-xs md:text-lg text-focus-in'>Firmes is a worldwide studio, specialising in content production. Our goal is to guide brands and personal brands through the constantly shifting digital world using a content-first marketing approach. We provide human-centric solutions that foster brand authenticity and create true value to communities</p>
                             </div>
 
@@ -28,10 +29,11 @@ export const ImagesMobileContainer = ({ data }) => {
                     )
                 } else {
                     return (
-                        <ImageBox key={project?.project_info_id} id={project?.project_info_id} title={project?.project_title} src={project.project_images[Math.floor(Math.random() * project.project_images.length)]?.project_image_url} alt={data[0]?.project_description} />
+                        <MobileImageBox key={project?.project_info_id} id={project?.project_info_id} title={project?.project_title} src={location.pathname !== "/work" ? project?.project_images.filter((image) => image.image_is_portrait)[0].project_image_url : project.project_images[Math.floor(Math.random() * project.project_images.length)]?.project_image_url} alt={data[0]?.project_description} />
                     )
                 }
             })}
+            
         </div>
     )
 }
