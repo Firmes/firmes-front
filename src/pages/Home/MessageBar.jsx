08c0234bb803f5581cccd9974b35bbe7bd19c3b2
rodridega <rodridega@gmail.com>
2023-05-02 15:43:24 -0300

    style: refactor font size to messageBar

--- src/pages/Home/MessageBar.jsx
+++ src/pages/Home/MessageBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const MessageBar = () => {
   return (
     <section className="h-8 md:flex w-full max-w-full bg-purple-700 text-firmes-black p-2 items-center justify-center fixed z-40  bottom-0 overflow-hidden ">
-      <p className="hidden  md:block text-xs text-center lg:text-xl xl:text-2xl font-extrabold">LIVE#&*ABSTRACT~IDEAS*%L4T1N0’;[CREATIVE CREATURES%%IMPACT SEEKERS&//CHANGEMAKERS+/.EMPATHY&LOVE]=1EVOLV3
+      <p className="hidden md:block text-xs text-center lg:text-base xl:text-xl font-extrabold">LIVE#&*ABSTRACT~IDEAS*%L4T1N0’;[CREATIVE CREATURES%%IMPACT SEEKERS&//CHANGEMAKERS+/.EMPATHY&LOVE]=1EVOLV3
       </p>
       <p className="animation-message-bar sm:hidden text-sm">
       BOGOTA ™£4& MADRID ^&81 NEW YORK #$(67 ||| LIVE NOW ON YOUTUBE STREAM ||| SALUDOS
